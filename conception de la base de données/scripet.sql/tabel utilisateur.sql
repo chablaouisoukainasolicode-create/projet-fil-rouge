a1bcceaeea7e6c9c117f610aacb37b19a54f8501
chablaouisoukainasolicode-create <chablaoui.soukaina.solicode@gmail.com>
2026-06-01 15:19:56 +0000

    Delete conception de la base de données/scripet.sql/tabel utilisateur.sql

--- conception de la base de données/scripet.sql/tabel utilisateur.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE utilisateurs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(50),
-    prenom VARCHAR(50),
-    email VARCHAR(100) UNIQUE,
-    mot_de_passe VARCHAR(100),
-    role ENUM('admin','user') DEFAULT 'user',
-    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
